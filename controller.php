12b2a76351d5247e7b498dca9071dbb86344934b
Oliver Green <green2go@gmail.com>
2016-03-07 23:05:37 +0000

    0.9.1

--- controller.php
+++ controller.php
@@ -16,7 +16,7 @@ class Controller extends Package
 
     protected $appVersionRequired = '5.7.1';
 
-    protected $pkgVersion = '0.9.0';
+    protected $pkgVersion = '0.9.1';
 
     public function getPackageName()
     {
